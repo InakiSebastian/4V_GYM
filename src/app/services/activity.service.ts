3b8cb401f653e6ae6ff011fdcfa82fd0be1a8b3f
Efontser <149145102+Efontser@users.noreply.github.com>
2024-12-30 19:25:42 +0100

    Update activity.service.ts

--- src/app/services/activity.service.ts
+++ src/app/services/activity.service.ts
@@ -1,9 +1,28 @@
 import { Injectable } from '@angular/core';
+import { Activity } from '../models/activity.model';
+import { InstructorService } from './instructor.service';
 
 @Injectable({
   providedIn: 'root'
 })
 export class ActivityService {
+  private activities: Activity[] = [];
 
-  constructor() { }
+  constructor(private instructorService: InstructorService) {
+    this.activities = this.generateActivities();
+  }
+  // BodyPump 2 Instructor || Spinning 1?
+  private generateActivities(): Activity[] {
+    const instructors = this.instructorService.getInstructors();
+
+    return [
+      new Activity("BodyPump", "Physical", new Date('2024-12-01T10:00:00'), [instructors[0], instructors[1]]),
+      new Activity("Spinning", "Physical", new Date('2024-12-01T12:00:00'), [instructors[2]]),
+      new Activity("Music Theory", "Educational", new Date('2024-12-01T14:00:00'), [instructors[3], instructors[4]]),
+    ];
+  }
+
+  public getActivities(): Activity[] {
+    return this.activities;
+  }
 }
