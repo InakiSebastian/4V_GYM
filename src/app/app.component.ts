9e1bff41db96f376dc994045992e3c3a53036e60
Efontser <149145102+Efontser@users.noreply.github.com>
2024-12-30 18:15:55 +0100

    CHANGE Minor AppComponent Adding Header

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,13 +2,15 @@ import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { TabSelectorComponent } from "./tab-selector/tab-selector.component";
 import { TabComponent } from "./tab/tab.component";
+import { HeaderComponent } from './header/header.component';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, TabSelectorComponent, TabComponent],
+  imports: [RouterOutlet, TabSelectorComponent, TabComponent, HeaderComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
 export class AppComponent {
-  title = '4V_GYM';
+  titleOrganization: string = '4V';
+  titleBusiness: string = 'GYM';
 }
