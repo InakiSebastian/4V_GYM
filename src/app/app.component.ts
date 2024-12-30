f6f25759822df23ffa56b047d0ac540a90ec0532
InakiSebastianDesktop <inakicabrerao@gmail.com>
2024-12-30 16:20:49 +0100

    Commit: Añadiendo tabSelector component y tab Component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { TabSelectorComponent } from "./tab-selector/tab-selector.component";
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, TabSelectorComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
---
+++ src/app/tab-selector/tab-selector.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-tab-selector',
+  imports: [],
+  templateUrl: './tab-selector.component.html',
+  styleUrl: './tab-selector.component.scss'
+})
+export class TabSelectorComponent {
+
+}
