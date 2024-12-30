7c62108532fc7892c465bb0fdd7b51b81daa97cf
Efontser <149145102+Efontser@users.noreply.github.com>
2024-12-30 19:25:37 +0100

    Update activity.model.ts

--- src/app/models/activity.model.ts
+++ src/app/models/activity.model.ts
@@ -3,11 +3,13 @@ import { Instructor } from "./instructor.model";
 export class Activity {
     activityName: string;
     activityType: string;
+    activityDate: Date;
     instructorList: Instructor[];
 
-    constructor(activityName: string, activityType: string, instructorList: Instructor[]) {
+    constructor(activityName: string, activityType: string,activityDate: Date, instructorList: Instructor[]) {
         this.activityName = activityName;
         this.activityType = activityType;
+        this.activityDate = activityDate;
         this.instructorList = instructorList;
     }
 
