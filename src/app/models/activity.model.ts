6d508038067326e1c5fd327545361cf0d7c4ee57
InakiSebastianDesktop <inakicabrerao@gmail.com>
2024-12-30 17:35:50 +0100

    Commit: Añadiendo atributos Models

--- src/app/models/activity.model.ts
+++ src/app/models/activity.model.ts
@@ -1,3 +1,17 @@
+import { Instructor } from "./instructor.model";
+
 export class Activity {
-    
+    activityName: string;
+    activityType: string;
+    instructorList: Instructor[];
+
+    constructor(activityName: string, activityType: string, instructorList: Instructor[]) {
+        this.activityName = activityName;
+        this.activityType = activityType;
+        this.instructorList = instructorList;
+    }
+
+    toString() {
+        return `Activity: ${this.activityName} of type: ${this.activityType} with instructors: ${this.instructorList}`;
+    }   
 }
--- src/app/models/instructor.model.ts
+++ src/app/models/instructor.model.ts
@@ -1,2 +1,15 @@
 export class Instructor {
+    name: String;
+    email: string;
+    telf: number;
+
+    constructor(name: string, email: string, telf: number) {
+        this.name = name;
+        this.email = email;
+        this.telf = telf;
+    }
+
+    toString() {
+        return `Instructor: ${this.name} with email: ${this.email} and telf: ${this.telf}`;
+    }
 }
